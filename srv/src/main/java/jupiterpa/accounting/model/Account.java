aa9d09b77925ee6dda32832a10dd1dc7fa2b3339
D043312 <D043312@WDFN34353748A.emea.global.corp.sap>
2018-10-03 13:35:26 +0200

    Added JPA

---
+++ srv/src/main/java/jupiterpa/accounting/model/Account.java
@@ -0,0 +1,45 @@
+package jupiterpa.accounting.model;
+
+import java.lang.Integer;
+import java.lang.String;
+import javax.annotation.Generated;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Generated("com.sap.cds.csn2jpa")
+@Table(
+    name = "jupiterpa_accounting_Account"
+)
+@Entity
+public class Account {
+  @Column(
+      nullable = false
+  )
+  @Id
+  private Integer ID;
+
+  @Basic
+  private String name;
+
+  public Account() {
+  }
+
+  public Integer getID() {
+    return this.ID;
+  }
+
+  public void setID(Integer ID) {
+    this.ID = ID;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
